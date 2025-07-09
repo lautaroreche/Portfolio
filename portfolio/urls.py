73f3aa4353014b5d22b63966d1d016ba9d29c3d4
Lautaro Reche <lautaroreche1@gmail.com>
2025-07-09 18:05:20 +0100

    Change url route while visit web portfolio

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -24,7 +24,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name = 'home'),
-    path('frontend/', frontend),
+    path('web/', frontend),
 ]
 
 
