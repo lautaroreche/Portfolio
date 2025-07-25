b955ec2ab4a379e802574e8284e8c07e9a0d9ebd
Lautaro Reche <lautaroreche1@gmail.com>
2025-07-25 09:51:57 +0100

    Add carousel to projects

--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
 from .models import Project, Technology, WorkExperience, SocialMedia
+import json
 
 
 def home(request):
+    images_json = {}
     projects = Project.objects.filter(type__in=['frontend', 'backend'])
     technologies = Technology.objects.all()
     work_experiences = WorkExperience.objects.all()
     social_media = SocialMedia.objects.all()
+
+    for project in projects:
+        image_urls = [project.image.url]
+        for image_field in ['image2', 'image3']:
+            image = getattr(project, image_field)
+            if image:
+                image_urls.append(image.url)
+        images_json[project.id] = json.dumps(image_urls)
 
     work_experience_description_dict = {}
     for work_experience in work_experiences:
@@ -14,6 +24,7 @@ def home(request):
 
     context = {
         "projects": projects,
+        "images_json": images_json,
         "technologies": technologies,
         "social_media": social_media,
         "work_experiences": work_experiences,
